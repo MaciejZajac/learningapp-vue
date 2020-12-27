3babc51e08e0fbd9aad019268553d56f7ea25a2c
Maciej Zając <maciej.zajac.197@gmail.com>
2020-12-27 22:53:59 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoredResources from './components/learning-resources/StoredResources.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { StoredResources },
+  data() {
+    return {
+      storedResources: [
+        { id: "official-guide", title: "Official Guide", description: "The official Vue.js documentation", link: "https://vuejs.org" },
+        { id: "google", title: "Google", description: "learn to google...", link: "https://google.com" }
+      ]
+    }
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
